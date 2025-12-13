3909548c0d61105d0a8cf22cab74f7f5cb6a00ee
NizariIddi <kn03nneez@gmail.com>
2025-12-14 00:11:24 +0300

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const path = require("path")
+
 const app = express();
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "../pubic")))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "../public", "index.html"))
+})
 
 // Routes
 app.use("/api/auth", require("./routes/auth.routes"));
